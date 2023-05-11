d0865267487b0089392d0954d07932fe5cbcb9af
ici <ici@DESKTOP-R9TEVC2>
2023-05-11 17:39:01 +0900

    페이징

--- src/main/java/com/psh/home/dto/Criteria.java
+++ src/main/java/com/psh/home/dto/Criteria.java
@@ -13,9 +13,4 @@ public class Criteria {
 	private int pageNum=1; //현재 페이지 번호(글 리스트 화면에서 무조건 1페이지가 보여져야하므로 초기값은1)
 	private int amount=5; //한 페이지에 출력표시될 글의 수
 	private int startNum; //현재 선택된 페이지에서 시작할 글 번호
-		
-	public String getQueryString() { //페이지 파라미터 값을 넘김
-		return String.format("page=%d&pageSize=%d", pageNum, amount);		
-	}
-	
 }
